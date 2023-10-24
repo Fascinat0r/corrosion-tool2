f03265cbd7294235eb45a8a93e9ca2392399dd31
nanatic <uliamurick@gmail.com>
2023-10-25 01:51:05 +0300

    Rebuild project
    - Add Excel importing by @Selskij
    - Remove useless dataclass annotation
    - Remove useless json importing
    - Refactor class by conversion some funtions to methods(finally)

--- Tube_point.py
+++ Tube_point.py
@@ -1,15 +1,14 @@
-from dataclasses import dataclass, field
 from typing import List
 
+import pandas as pd
 from thermopack.cubic import cubic
 
 from PVT import *
 
 
-@dataclass
 class Tube_point:
     name: str = 'mixture'  # name: string, name of the material
-    phase_name: List[str] = field(default_factory=lambda: ['gas'])  # phase_name: list of strings, name of phases
+    phase_name: List[str] = ['gas']  # phase_name: list of strings, name of phases
     number_of_fluids: int = 1  # number_of_fluids: integer, number of phases
     temperature: float = 300.0  # T: double, local temperature, Kelvin
     pressure: float = 101325.0  # p: double, local pressure, Pascal
@@ -17,23 +16,20 @@ class Tube_point:
     diameter: float = 0.1  # D: double, tube local diameter, m
     length: float = 10  # length: double, tube length, m. Zero if it's endpoint
     section_type = 1  # section type: integer, type of the section for losses calculation, not used yet - enum!
-    molar_composition: List[float] = field(
-        default_factory=lambda: [1.0])  # molar_composition: list of doubles, molar composition [probably constant]
-    molar_masses: List[float] = field(
-        default_factory=lambda: [1.0])  # molar_masses: list of doubles, molar masses [constant overall]
-    vapor_components: List[float] = field(default_factory=lambda: [
-        0.5])  # vapor_components: list of doubles, vapor distribution over components (sum is 1)
-    liquid_components: List[float] = field(default_factory=lambda: [
-        0.5])  # liquid_components: list of doubles, liquid distribution over components (sum is 1)
-    components_density: List[float] = field(
-        default_factory=lambda: [1.0])  # components_density: list of doubles, density distribution over components
+    molar_composition: List[float] = [1.0]  # molar_composition: list of doubles, molar composition [probably constant]
+    molar_masses: List[float] = [1.0]  # molar_masses: list of doubles, molar masses [constant overall]
+    vapor_components: List[float] = [
+        0.5]  # vapor_components: list of doubles, vapor distribution over components (sum is 1)
+    liquid_components: List[float] = [
+        0.5]  # liquid_components: list of doubles, liquid distribution over components (sum is 1)
+    components_density: List[float] = [1.0]  # components_density: list of doubles, density distribution over components
     overall_density: float = 1.0  # overall_density: double, overall density, kg/m^3
     overall_vapor_fraction: float = 0.5  # overall_vapor_fraction: double, vapor distribution over mixture
     overall_liquid_fraction: float = 0.5  # overall_liquid_fraction: double, liquid distribution over mixture
-    liquid_viscosities: List[float] = field(default_factory=lambda: [
-        1.0e-3])  # liquid_viscosities: list of doubles, viscosity of liquid parts over components
-    vapor_viscosities: List[float] = field(default_factory=lambda: [
-        1.0e-3])  # vapor_viscosities:  list of doubles, viscosity of vapor parts over components
+    liquid_viscosities: List[float] = [
+        1.0e-3]  # liquid_viscosities: list of doubles, viscosity of liquid parts over components
+    vapor_viscosities: List[float] = [
+        1.0e-3]  # vapor_viscosities:  list of doubles, viscosity of vapor parts over components
     liquid_overall_viscosity: float = 1.0e-3  # liquid_overall_viscosity: double, viscosity of liquid part
     vapor_overall_viscosity: float = 1.0e-3  # vapor_overall_viscosity: double, viscosity of vapor part
     overall_viscosity: float = 1.0e-3  # overall_viscosity: double, viscosity of mixture
@@ -41,11 +37,39 @@ class Tube_point:
     flow_mode_key: float = 1.0  # flow_mode_key: double, currently XTT, later - other number to characterize flow_mode
     flow_mode_friction_factor: float = 1.0  # flow_mode_friction_factor: double, currently from XTT
     reynolds_number: float = 10000.0  # reynolds_number: double, Reynolds number for ...
+    roughness = 0.01  # roughness: шероховатость внутренней поверхности трубы
+    mass = 0.1
+    angle = 0  # angle: угол наклона трубы
+
+    def start_point_from_excel(self, excel_path):  # initialization of start pointы
+        df = pd.read_excel(excel_path)
+        _ = df['Unnamed: 1']
+
+        self.temperature = _[0]
+        self.pressure = _[2]
+        self.molar_composition = [_[7], _[13]]
+        self.molar_masses = [_[6], _[12]]
+        self.velocity = _[3]
+        self.diameter = _[22]
+        self.length = _[20]
+        self.vapor_viscosities = _[9]
+        self.liquid_viscosities = _[15]
+        self.components_density = [_[8], _[14]]
+        self.roughness = _[21]
+        self.mass = _[1]
+        self.update_point_state()
+
+    def define_tube_params(self, diameter, length, density_old):
+        q = self.velocity * self.diameter * self.diameter * density_old
+        new_velocity = q / (diameter * diameter * self.overall_density)  # mass balance, pi/4 is skipped
+        # due to presence in both parts of equation
+        self.diameter = diameter
+        self.length = length
+        self.velocity = new_velocity
 
     def update_point_state(self):
         """
         Updates tube point parameters after changing local temperature and pressure
-        :return: tube point with the updated state
         """
         rk_fluid = cubic('N2,ETOH', 'SRK')  # obsolete
         x, y, vap_frac, liq_frac, phase_key = rk_fluid.two_phase_tpflash(self.temperature, self.pressure,
@@ -103,30 +127,10 @@ class Tube_point:
         xi = self.calculate_lambda() * self.length / self.diameter
         return (xi * self.velocity ** 2) * 0.5 * self.overall_density
 
-
-def calculate_xtt(liquid_density, gas_density, liquid_viscosity, gas_viscosity, velocity, diameter):
-    return ((1.096 / liquid_density) ** 0.5) * ((liquid_density / gas_density) ** 0.25) * (
-            (gas_viscosity / liquid_viscosity) ** 0.1) * ((velocity / diameter) ** 0.5)
-
-
-def calculate_viscosity(liquid_viscosity, gas_viscosity, friction_factor):
-    return friction_factor * liquid_viscosity + (1 - friction_factor) * gas_viscosity
-
-
-def calculate_Re(velocity, diameter, overall_density, overall_viscosity):
-    return velocity * diameter * overall_density / overall_viscosity
-
-
-def calculate_lambda(Re):
-    if Re < 2300:
-        return 64 / Re
-    else:
-        return 0.316 / (Re ** 0.25)
-
-
-def return_pressure_loss(velocity, diameter, length, lam, density):
-    xi = lam * length / diameter
-    return (xi * velocity ** 2) * 0.5 * density
+    def calculate_viscosity(self, friction_factor):
+        liquid_viscosity = self.liquid_viscosities[0]  # ? liquid_overall_viscosity?
+        gas_viscosity = self.liquid_viscosities[1]  # ?
+        return friction_factor * liquid_viscosity + (1 - friction_factor) * gas_viscosity
 
 
 def return_mode(xtt):
@@ -162,12 +166,3 @@ def return_friction_factor(xtt):
         return 0.6
     return 0
 
-
-def define_tube_params(point: Tube_point, diameter, length, density_old):
-    q = point.velocity * point.diameter * point.diameter * density_old
-    new_velocity = q / (diameter * diameter * point.overall_density)  # mass balance, pi/4 is skipped
-    # due to presence in both parts of equation
-    point.diameter = diameter
-    point.length = length
-    point.velocity = new_velocity
-    return point
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 
 sys.path.insert(0, '../pycThermopack/')
 from Tube_point import *
-from start_point import start_point
 
 
 def pvt_block(point: Tube_point, new_pressure, new_temperature):
@@ -16,7 +15,7 @@ def pvt_block(point: Tube_point, new_pressure, new_temperature):
 
 def main(path: str):
     point = Tube_point()
-    start_point(point, path)
+    point.start_point_from_excel(path)
     tube_diameters = [0.08, 0.08, 0.08, 0.09, 0.09, 0.09, 0.09, 0.08, 0.08, 0.1]
     tube_lengths = [1000, 200, 450, 1000, 200, 300, 200, 300, 1000, 0]
     tube_points = [point]
@@ -31,13 +30,11 @@ def main(path: str):
         flow_mode = return_mode(xtt)
         print('Found ', flow_mode, ' flow_mode at', i, '! xtt= ', xtt)
         friction_factor = return_friction_factor(xtt)
-        tube_points[i - 1].overall_viscosity = calculate_viscosity(tube_points[i - 1].liquid_viscosities[0],
-                                                                   tube_points[i - 1].liquid_viscosities[1],
-                                                                   friction_factor)
+        tube_points[i - 1].overall_viscosity = tube_points[i - 1].calculate_viscosity(friction_factor)
         tube_points[i - 1].reynolds_number = tube_points[i - 1].calculate_Re()
         re_nums.append(tube_points[i - 1].reynolds_number)
         print('Reynolds number for ', i, ' is ', tube_points[i - 1].reynolds_number, 'lambda is ',
-              calculate_lambda(tube_points[i - 1].reynolds_number))
+              tube_points[i - 1].calculate_lambda())
         diff = point.calculate_pressure_loss()
         P1 = tube_points[i - 1].pressure - diff
         T1 = tube_points[i - 1].temperature - i * 0.3
@@ -48,59 +45,14 @@ def main(path: str):
 
         density = next_point.overall_density
         next_point = pvt_block(next_point, P1, T1)
-        next_point = define_tube_params(next_point, tube_diameters[i], tube_lengths[i], density)
+        next_point.define_tube_params(tube_diameters[i], tube_lengths[i], density)
 
         tube_points.append(next_point)
         print()
 
 
-# end of PVT block
-# start of flow_mode part
-# xtt = calculate_xtt(p)
-# flow_mode = return_mode(xtt)
-# print ('Found ', flow_mode, ' flow_mode!; xtt= ', xtt)
-# end of flow_mode part
-# start of friction part
-# ff = return_friction_factor(xtt)
-
-# p.overall_viscosity = calculate_visc(p.liquid_viscosities[0], p.liquid_viscosities[1], ff)
-
-# p.Re = calculate_Re(p)
-
-# print ('Reynolds number is ', p.Re)
-# diff = return_pressure_loss(p.v,p.D,length, return_lambda(p.Re), p.overall_density)
-# print (p.p, diff)
-# P1 = p.p-diff
-# T1 = p.T-20.0
-# print ('New pressure is', P1)
-
-# p2 = copy.deepcopy(p)
-# pvt_block(p2, P1, T1)
-# xtt2 = calculate_xtt(p2)
-# mode2 = return_mode(xtt2)
-# print ('New flow_mode is ', mode2, ' !; xtt= ', xtt2)
-# pressure loss calculated, now time for calculating flow_mode again
-# print ('dp', p.p, P1, 'D', p.D, 'length', length)
-# wss = 0.25*(diff)*p.D/length
-# print ('Average WSS is about', wss, 'Pa')
-
-# tube = [p,p2]
-
-# rho_1_1, =  rk_fluid.specific_volume(T1, P1, z, 1)
-# rho_1_1= m_ethanol/rho_1_1
-# rho_2_1, =  rk_fluid.specific_volume(T1, P1, z, 2)
-# rho_2_1 = m_n2/rho_2_1
-# rho_tot_1 = z[0]*rho_1+z[1]*rho_2
-
-
-# xtt = calculate_xtt(rho_1_1, rho_2_1, mu_1, mu_2, v1, D)
-# flow_mode = return_mode(xtt)
-# print ('New flow_mode is ', flow_mode, ' !; xtt= ', xtt)
-# wss = 0.25*(p-P1)*D/length
-# print ('Average WSS is about', wss, 'Pa')
-
 if __name__ == "__main__":
     start = time.time()
-    main("jsons/default.json")
+    main("../xlsm/Imput_data.xlsm")
     end = time.time()
     print("The time of execution of above program is :", (end - start) * 10 ** 3, "ms")
