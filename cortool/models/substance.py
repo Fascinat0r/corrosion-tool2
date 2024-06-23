123da726354caafd253bb3a96e3f2dce3c12f32e
Fascinat0r <fascinator2904@gmail.com>
2024-06-23 12:26:34 +0300

    Update substances
    - create dict of all substances automatically
    - add name to substance

--- cortool/models/substance.py
+++ cortool/models/substance.py
@@ -3,14 +3,24 @@ from abc import ABC, abstractmethod
 
 
 class Substance(ABC):
+    name: str
+    molar_mass: float  # Молярная масса вещества в г/моль
+
+    def __init__(self, name, molar_mass: float):
+        self.name = name
+        self.molar_mass = molar_mass
+
     @abstractmethod
-    def viscosity_from_temperature(self, temperature: float) -> float:
+    def get_viscosity(self, temperature: float) -> float:
         """Метод для расчёта вязкости в зависимости от температуры"""
         pass
 
 
 class Ethanol(Substance):
-    def viscosity_from_temperature(self, temperature: float) -> float:
+    def __init__(self):
+        super().__init__(name="ethanol", molar_mass=46.07)
+
+    def get_viscosity(self, temperature: float) -> float:
         A = 0.00201 * 1e-6
         B = 1614
         C = 0.00618
@@ -19,16 +29,24 @@ class Ethanol(Substance):
 
 
 class Nitrogen(Substance):
-    def viscosity_from_temperature(self, temperature: float) -> float:
+    def __init__(self):
+        super().__init__(name="nitrogen", molar_mass=28.02)
+
+    def get_viscosity(self, temperature: float) -> float:
         VISCOSITY_INIT = 1.7e-5
         T_INIT = 273
         S = 104.7
         return (VISCOSITY_INIT * (temperature / T_INIT) ** 1.5) * (T_INIT + S) / (temperature + S)
 
 
-# Пример:
-substances = list()
-substances.append(Ethanol())
-substances.append(Nitrogen())
-for sub in substances:
-    print(print(f"Viscosity of {sub.__class__} at 300K:", sub.viscosity_from_temperature(300)))
+def create_substance_dictionary():
+    substance_dict = {}
+    for subclass in Substance.__subclasses__():
+        instance = subclass()
+        substance_dict[instance.name] = instance
+    return substance_dict
+
+
+# Использование словаря
+substance_dict = create_substance_dictionary()
+print("All available substances:", list(substance_dict.keys()))
